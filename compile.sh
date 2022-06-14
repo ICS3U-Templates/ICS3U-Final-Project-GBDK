13cf73b11f96cf66f04241098d71807c3d518843
Patrick Coxall <mr.coxall@mths.ca>
2022-06-14 14:58:32 -0400

    update

--- compile.sh
+++ compile.sh
@@ -4,11 +4,13 @@
 # Created on: Jun 2022
 # Created for: To automate the generation of gameboy files
 
+base_name=$(basename "$1" .c)
+
 # remove old gameboy file from the simulator directory
-rm ~/GBDK/simulator/rom/*.gb
+rm ~/GBDK/simulator/rom/game.gb
 
 # compile to game
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -o $1.gb $1.c
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -o $base_name.gb $base_name.c
 
 # move it to the simulator directory
-cp ./$1.gb ~/GBDK/simulator/rom/$1.gb
+cp ./$base_name.gb ~/GBDK/simulator/rom/game.gb
