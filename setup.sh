3941775c09bfc45b86bb3e6e51385a8d0c8561c7
Patrick Coxall <mr.coxall@mths.ca>
2022-06-14 15:01:09 -0400

    update

--- setup.sh
+++ setup.sh
@@ -52,6 +52,19 @@ read  any_key
 
 # create an GBDK directory in the root directory
 mkdir ~/GBDK
+mkdir ~/GBDK/lesson-01
+mkdir ~/GBDK/lesson-02
+mkdir ~/GBDK/lesson-03
+mkdir ~/GBDK/lesson-04
+mkdir ~/GBDK/lesson-05
+mkdir ~/GBDK/lesson-06
+mkdir ~/GBDK/lesson-07
+mkdir ~/GBDK/lesson-08
+mkdir ~/GBDK/lesson-09
+mkdir ~/GBDK/lesson-10
+mkdir ~/GBDK/lesson-11
+mkdir ~/GBDK/lesson-12
+mkdir ~/GBDK/lesson-13
 
 # copy over the .vscode directory and web tools into it
 cp -r .vscode ~/GBDK/.vscode
