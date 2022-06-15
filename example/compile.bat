80d7959300b3e787eb68a72fae7e70c9fdab8cce
Patrick Coxall <mr.coxall@mths.ca>
2022-06-15 08:14:04 -0400

    update

--- example/compile.bat
+++ example/compile.bat
@@ -1,8 +1,5 @@
 # this bat file will compile the C program to *.gb
 
-# remove old gameboy file from the simulator directory
-rm ~/gbdk/simulator/rom/*.gb
-
 # compile to game
 /opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -o game.gb hello_world.c
 
